703f7dfe30d44f5205ea3a45aa7c05a12d826088
Pavlo <dneprpi@gmail.com>
2021-08-11 17:05:04 +0300

    edited wallets

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Index';
+import Google from './components/google/Google';
+import Wallets from './components/Wallets/Wallets';
 
 function App() {
   return (
     <>
+    
      <Router>
+       <Switch></Switch>
        <Home />
      </Router>
-
+     <Google />
+     <Wallets/>
     </>
   );
 }
