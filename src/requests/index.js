89f28d8124f09c0081ba627726cbcd8613bc5e1b
Pavlo <dneprpi@gmail.com>
2021-08-20 16:11:07 +0300

    edit account page

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,29 +1,41 @@
 import axios from "axios";
 
-const BACK_URL = 'http://localhost:1234';
+const BACK_URL = "http://localhost:5050";
 
-export const registerUser = data => {
-    return axios({
-        method: 'post',
-        baseURL: BACK_URL,
-        url: '/api/Auth/registration',
-        headers: {
-            'Content-Type': 'application/json'
-                // "Access-Control-Allow-Origin": "*"
-        },
-        data
-    });
-}
+export const registerUser = (data) => {
+  return axios({
+    method: "post",
+    baseURL: BACK_URL,
+    url: "/api/Auth/registration",
+    headers: {
+      "Content-Type": "application/json",
+      //   "Access-Control-Allow-Origin": "*",
+    },
+    data,
+  });
+};
 
-export const singInUser = data => {
-    return axios({
-        method: 'post',
-        baseURL: BACK_URL,
-        url: '/api/Auth/login',
-        headers: {
-            'Content-Type': 'application/json'
-                // "Access-Control-Allow-Origin": "*"
-        },
-        data
-    });
-}
+export const singInUser = (data) => {
+  return axios({
+    method: "post",
+    baseURL: BACK_URL,
+    url: "/api/Auth/login",
+    headers: {
+      "Content-Type": "application/json",
+      // "Access-Control-Allow-Origin": "*"
+    },
+    data,
+  });
+};
+
+export const getUser = (leadID) => {
+  return axios({
+    method: "get",
+    baseURL: BACK_URL,
+    url: "/api/lead/" + leadID,
+    headers: {
+      "Content-Type": "application/json",
+      // "Access-Control-Allow-Origin": "*"
+    },
+  });
+};
